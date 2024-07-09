3b8a2b37be4f0c5b390133c43767d744e3906e61
Hoang Nguyen <hoangnhh140697@gmail.com>
2024-07-09 20:51:27 +0700

    chore: update loading process after payment success

--- packages/order_food_ui/lib/pages/payment-success.dart
+++ packages/order_food_ui/lib/pages/payment-success.dart
@@ -2,57 +2,77 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-class PaymentSuccessPage extends StatelessWidget {
+class PaymentSuccessPage extends StatefulWidget {
   final Map<String, String> queryParams;
 
   PaymentSuccessPage({required this.queryParams});
 
   @override
-  Widget build(BuildContext context) {
-    
-    final String? code = queryParams['code'];
-    final String? id = queryParams['id'];
-    final String? cancel = queryParams['cancel'];
-    final String? status = queryParams['status'];
-    final String? orderCode = queryParams['orderCode'];
+  _PaymentSuccessPageState createState() => _PaymentSuccessPageState();
+}
 
-    // Check if the cancel status is true and status is CANCELLED
-    bool isPaid = cancel == 'false' && status == 'PAID';
+class _PaymentSuccessPageState extends State<PaymentSuccessPage> {
+  bool isLoading = false;
+  bool isPaid = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final String? cancel = widget.queryParams['cancel'];
+    final String? status = widget.queryParams['status'];
+
+    // Check if the cancel status is true and status is PAID
+    isPaid = cancel == 'false' && status == 'PAID';
 
     if (isPaid) {
-      // Call the API
-      _checkTransactionSuccessed(queryParams);
+      _checkTransactionSuccessed(widget.queryParams);
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String? orderCode = widget.queryParams['orderCode'];
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Payment Cancel - OrderCode: $orderCode'),
       ),
       body: Center(
-        child: isPaid
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.network('assets/payment-success.webp'),
-                  SizedBox(height: 16),
-                  Text(
-                    'Payment has been successful',
-                    style: TextStyle(
-                        color: Colors.green[600],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 23),
-                  ),
-                ],
-              )
-            : Text('Invalid cancellation status'),
+        child: isLoading
+            ? CircularProgressIndicator()
+            : isPaid
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network('assets/payment-success.webp'),
+                      SizedBox(height: 16),
+                      Text(
+                        'Payment has been successful',
+                        style: TextStyle(
+                            color: Colors.green[600],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 23),
+                      ),
+                    ],
+                  )
+                : Text('Invalid cancellation status'),
       ),
     );
   }
 
   void _checkTransactionSuccessed(Map<String, String?> queryParams) async {
+    setState(() {
+      isLoading = true;
+    });
+
     final uri = Uri.http(
         dotenv.env['API_DOMAIN'] ?? '', '/api/payment/callback', queryParams);
     final response = await http.get(uri);
+
+    setState(() {
+      isLoading = false;
+    });
 
     if (response.statusCode == 200) {
       print('Transaction cancelled successfully');
